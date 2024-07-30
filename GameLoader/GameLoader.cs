8093fc01fe2af585c383de86e8480c2aea0e240d
12Acorns <nathanrutter09@gmail.com>
2024-07-30 01:28:28 +0100

    Abstracted running a game through being handled by GameLoader class

--- GameLoader/GameLoader.cs
+++ GameLoader/GameLoader.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection;
+﻿using System.Globalization;
+using System.Reflection;
+using System.Text;
 
 namespace GameLoader;
 
@@ -15,12 +17,50 @@ public sealed class GameLoader
 		var _assignable = _assemblies.Where(type => typeof(IGame).IsAssignableFrom(type));
 
 		games = _assignable.Select(type => (IGame)Activator.CreateInstance(type)!);
-	}
+		gameNames = games.ToDictionary(x => x.Name, x => x);
 
+		var _builder = new StringBuilder("Enter desired game (Case-Sensitive): \n");
+		var _format = CompositeFormat.Parse("    -{0}\n");
+		var _culture = CultureInfo.InvariantCulture;
+
+		foreach(var _game in games)
+		{
+			_builder.AppendFormat(_culture, _format, _game.Name);
+		}
+
+		gameList = _builder.ToString();
+	}
+	
 	private static readonly string gamesDirectory =
 		Path.Combine(
 		AppDomain.CurrentDomain.BaseDirectory,
 		"Games");
 
-	public IEnumerable<IGame> games { get; }
+	private readonly Dictionary<string, IGame> gameNames;
+	private readonly IEnumerable<IGame> games;
+	private readonly string gameList;
+
+
+	public void Run()
+	{
+		Console.Write(gameList);
+		Console.Write('>');
+
+		var _runGame = AskGame();
+		_runGame.Run();
+	}
+
+	private IGame AskGame()
+	{
+		var _desiredGame = Console.ReadLine() ?? "";
+
+		if(!gameNames.TryGetValue(_desiredGame, out var _game))
+		{
+			Console.WriteLine("\nPlease enter a valid game name!\n");
+			Console.Write(gameList);
+			Console.Write('>');
+			return AskGame();
+		}
+		return _game;
+	}
 }
